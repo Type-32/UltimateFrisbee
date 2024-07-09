573f51cd2a921b4064145a1485e1efdcc743bf45
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-07-10 00:33:21 +0800

    Changed Error Page Slightly
    Signed-off-by: Type-32 <87076491+Type-32@users.noreply.github.com>

--- pages/error.vue
+++ pages/error.vue
@@ -1,29 +1,35 @@
 <script setup lang="ts">
-
-import MainLayout from "~/layouts/MainLayout.vue";
-
 import type { NuxtError } from '#app'
-
-defineProps<{
-    error: NuxtError
-}>()
 
 useSeoMeta({
     title: 'Page not found',
     description: 'We are sorry but this page could not be found.'
 })
+
+defineProps({
+    error: {
+        type: Object as PropType<NuxtError>,
+        required: true
+    }
+})
+
+useHead({
+    htmlAttrs: {
+        lang: 'en'
+    }
+})
 </script>
 
 <template>
-    <UContainer>
+    <div>
         <UMain>
-            <UPage>
-                <UPageError :error="error" />
-            </UPage>
+            <UContainer>
+                <UPage>
+                    <UPageError :error="error" />
+                </UPage>
+            </UContainer>
         </UMain>
-    </UContainer>
+
+        <UNotifications />
+    </div>
 </template>
-
-<style scoped>
-
-</style>
