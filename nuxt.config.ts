691bc4ea431e01cd3535f482f5662ff134165054
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-07-12 16:41:25 +0800

    Using Nuxt 4 Compatibility Version
    Signed-off-by: Type-32 <87076491+Type-32@users.noreply.github.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
+
   devtools: { enabled: true },
   extends: ['@nuxt/ui-pro'],
 
